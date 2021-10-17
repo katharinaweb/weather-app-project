41c7fe5f249422a9dc8c75f4162d3d05c3c3109d
katharinaweb <92152458+katharinaweb@users.noreply.github.com>
2021-10-17 17:54:55 +0200

    Fetched city, temperature and main weather details from API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 // API integration
 function showWeather(response) {
   console.log(response);
+  let currentCity = response.data.name;
+  let currentTemperature = response.data.main.temp;
+  let currentHumidity = response.data.main.humidity;
+  let currentWind = response.data.wind.speed;
+  let currentDescription = response.data.weather[0].description;
+  let cityElement = document.querySelector("#city-element");
+  cityElement.innerHTML = currentCity;
+  let temperatureElement = document.querySelector("#temperature-element");
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+  let humidityElement = document.querySelector("#humidity-element");
+  humidityElement.innerHTML = currentHumidity;
+  let windElement = document.querySelector("#wind-element");
+  windElement.innerHTML = currentWind;
+  let descriptionElement = document.querySelector("#description-element");
+  descriptionElement.innerHTML = currentDescription;
 }
 
 let apiKey = "210d99196a88b9257ed8cb3535a0a0c5";
