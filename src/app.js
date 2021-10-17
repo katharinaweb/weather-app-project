69aed2fcc47133f20876344a53a2ec50d6400d66
katharinaweb <92152458+katharinaweb@users.noreply.github.com>
2021-10-17 17:35:30 +0200

    Installed axios and integrated API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+// API integration
+function showWeather(response) {
+  console.log(response);
+}
 
+let apiKey = "210d99196a88b9257ed8cb3535a0a0c5";
+let apiCity = "Vienna";
+let apiUnit = "metric";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${apiCity}&appid=${apiKey}&units=${apiUnit}`;
+
+axios.get(apiUrl).then(showWeather);
