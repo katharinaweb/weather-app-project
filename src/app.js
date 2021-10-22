ab1199c9f62b0832f91e6bb683897d1d59c9ddbc
katharinaweb <92152458+katharinaweb@users.noreply.github.com>
2021-10-22 11:34:40 +0200

    Included Date and Time (incl. formatting)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,42 @@
 // API integration
+function formatDate(timestamp) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDate = new Date(timestamp);
+  let day = currentDate.getDay();
+  let date = currentDate.getDate();
+  if (date < 10) {
+    date = `0${day}`;
+  }
+  let month = currentDate.getMonth() + 1;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  let hours = currentDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${weekdays[day]}, ${date}.${month}, ${hours}:${minutes}`;
+}
+
 function showWeather(response) {
   let currentCity = response.data.name;
   let currentTemperature = response.data.main.temp;
   let currentHumidity = response.data.main.humidity;
   let currentWind = response.data.wind.speed;
   let currentDescription = response.data.weather[0].description;
+  let currentTimestamp = response.data.dt * 1000;
   let cityElement = document.querySelector("#city-element");
   cityElement.innerHTML = currentCity;
   let temperatureElement = document.querySelector("#temperature-element");
@@ -15,6 +47,8 @@ function showWeather(response) {
   windElement.innerHTML = currentWind;
   let descriptionElement = document.querySelector("#description-element");
   descriptionElement.innerHTML = currentDescription;
+  let dateElement = document.querySelector("#date-element");
+  dateElement.innerHTML = formatDate(currentTimestamp);
 }
 
 let apiKey = "210d99196a88b9257ed8cb3535a0a0c5";
